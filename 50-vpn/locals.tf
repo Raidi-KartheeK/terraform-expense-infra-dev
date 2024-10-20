b49f2ccf78dda5baaaae6072d990e830c381cf94
Your Name <you@example.com>
2024-10-20 19:09:18 +0530

    msg

--- 50-vpn/locals.tf
+++ 50-vpn/locals.tf
@@ -1,6 +1,6 @@
 locals {
     resource_name = "${var.project_name}-${var.enivronment}-vpn"
     vpn_sg_id = data.aws_ssm_parameter.vpn_sg_id.value
-    public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
+    public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
 }
 
